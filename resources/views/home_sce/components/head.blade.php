5d00f7f660ab69d28adedd33b3cd90b2d593443a
Shawn Wang <wang77289271@gmail.com>
2022-11-24 08:28:09 -0700

    update favicon icon

--- resources/views/home_sce/components/head.blade.php
+++ resources/views/home_sce/components/head.blade.php
@@ -8,6 +8,7 @@
       href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
       rel="stylesheet"
     />
+    <link rel="icon" type="image/x-icon" href="home_sce/favicon.ico">
     <link rel="stylesheet" href="home_sce/css/style.css" />
     <link rel="stylesheet" href="home_sce/css/applicationform.css" />
     <link rel="stylesheet" href="home_sce/css/sce_programs.css" />
